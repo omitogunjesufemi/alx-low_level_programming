b082a48ca6472f1f78891c57c43faf0f6210cb5f
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-08 23:31:47 +0000

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The main method
+ * Return: int 1
+ */
+int main(void)
+{
+	char myString[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	puts(myString);
+	return (0);
+}
