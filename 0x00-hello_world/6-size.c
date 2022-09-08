ce446632db9c96027a1485c859be24a41b1546d1
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-08 21:41:13 +0000

    Print size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main method
+ * Return: int 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long d;
+	float e;
+
+	printf("Size of a char: %ld byte(s).\n", sizeof(a));
+	printf("Size of a int: %ld byte(s).\n", sizeof(b));
+	printf("Size of a long int: %ld byte(s).\n", sizeof(c));
+	printf("Size of a long long: %ld byte(s).\n", sizeof(d));
+	printf("Size of a float: %ld byte(s).\n", sizeof(e));
+	return (0);
+}
--- 0x00-hello_world/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("Hello, main!\n");
-	return (0);
-}
