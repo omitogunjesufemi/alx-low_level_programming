186614f13b7f243dc916a44d82c7fc277b054cc4
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-08 21:41:13 +0000

    Print size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %ld byte(s) \n", sizeof(a));
-	printf("Size of a int: %ld byte(s) \n", sizeof(b));
+	printf("Size of an int: %ld byte(s) \n", sizeof(b));
 	printf("Size of a long int: %ld byte(s) \n", sizeof(c));
 	printf("Size of a long long: %ld byte(s) \n", sizeof(d));
 	printf("Size of a float: %ld byte(s) \n", sizeof(e));
