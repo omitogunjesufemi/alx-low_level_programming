fc50d693662a8f68a91f5bcff51d8f62f9edf7c1
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-13 22:16:42 +0000

    Prints all alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
