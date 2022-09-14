bd02b63b5a70209c77d719c5315eb00e82911b28
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-14 16:47:12 +0000

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,18 +6,30 @@
  */
 int main(void)
 {
-	unsigned long f0 = 0, f1 = 1;
+	unsigned long int f0 = 0, f1 = 1, k, l;
 	int i;
 
 	for (i = 0; i < 98; i++)
 	{
-		unsigned long temp = f1;
+		unsigned long int temp = f1;
 
 		f1 = f1 + f0;
-		if (i == 97)
-			printf("%lu\n", f1);
+		if ((i >= 91) && (i < 97))
+		{
+			k = f1 % 100000000000;
+			l = f1 / 100000000000;
+			printf("%lu%lu, ", l, k);
+		}
+		else if (i == 97)
+		{
+			k = f1 % 100000000000;
+			l = f1 / 100000000000;
+			printf("%lu%lu", l, k);
+		}
 		else
+		{
 			printf("%lu, ", f1);
+		}
 		f0 = temp;
 	}
 	return (0);
