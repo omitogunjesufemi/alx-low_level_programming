83ccd1d578452dea0487441c927ff0c5d0b365e6
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-14 02:00:10 +0000

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void decreasing_counter(int count);
+void increasing_counter(int count);
+
+/**
+ * print_to_98 - prints all natural number from @n to 98
+ * @n: starting point
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		increasing_counter(n);
+		printf("\n");
+	}
+	else
+	{
+		decreasing_counter(n);
+		printf("\n");
+	}
+}
+
+
+/**
+ * decreasing_counter - counts from a number higher than 98
+ * @count: the beginning of the count
+ */
+void decreasing_counter(int count)
+{
+	int i;
+
+	for (i = count; i >= 98; i--)
+	{
+		if (i == 98)
+			printf("%d", i);
+		else
+			printf("%d, ", i);
+	}
+}
+
+
+/**
+ * decreasing_counter - counts from a number higher than 98
+ * @count: the beginning of the count
+ */
+void increasing_counter(int count)
+{
+	int i;
+
+	for (i = count; i <= 98; i++)
+	{
+		if (i == 98)
+			printf("%d", i);
+		else
+			printf("%d, ", i);
+	}
+}
