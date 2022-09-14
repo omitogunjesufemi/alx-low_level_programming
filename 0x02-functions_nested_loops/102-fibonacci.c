7013046ad1868df27c79c6961417fe5465cc58ae
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-14 03:52:00 +0000

    Fibonacci Series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main method
+ * Return: int
+ */
+int main(void)
+{
+	long int f0 = 0, f1 = 1;
+	int i;
+
+	for (i = 0; i < 50; i++)
+	{
+		long int temp = f1;
+
+		f1 = f1 + f0;
+		if(i == 49)
+			printf("%ld\n", f1);
+		else
+			printf("%ld, ", f1);
+		f0 = temp;
+	}
+	return (0);
+}
