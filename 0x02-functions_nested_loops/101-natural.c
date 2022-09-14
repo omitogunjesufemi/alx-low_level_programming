d549c655d973b97ae1e9e628e01f359e2173d158
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-14 03:12:46 +0000

    Print natural numbers of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * natural - multiples of 3 and 5
+ * Return - int
+ */
+int natural(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	return (sum);
+}
+
+/**
+ * main - main method
+ * Return: int
+ */
+int main(void)
+{
+	long int r;
+
+	r = natural();
+	printf("%ld\n", r);
+}
