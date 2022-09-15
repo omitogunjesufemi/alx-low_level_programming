c085f645859ce9dcde22868b029a797d525d8a6c
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-15 11:20:48 +0000

    Check for a digit 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int
+ * Return: int
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,3 +7,8 @@ int _putchar(char c);
  * This checks for uppercase characters
  */
 int _isupper(int c);
+
+/**
+ * This checks for a digit (0 through 9)
+ */
+int _isdigit(int c);
