ac94b5aed84fc3e2787d7fb783e67534bc1ac194
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-19 23:26:32 +0000

    Copy string pointed to by src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int str_len(char *s);
+
 /**
  * _strcpy - copying string pointed to by src to dest
  * @dest: destination
@@ -9,13 +11,30 @@
 char *_strcpy(char *dest, char *src)
 {
 	static char *output;
-	int i;
+	int i, len;
 
-	for (i = 0; src[i] != '\0'; i++)
+	len = str_len(src);
+	for (i = 0; i <= len; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[-1] = '\0';
 	output = dest;
 	return (output);
+}
+
+/**
+ * str_len - the length of a string
+ * @s: string
+ * Return: int
+ */
+int str_len(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
