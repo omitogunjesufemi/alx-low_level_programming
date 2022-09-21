2d8d6c18c682f81269badc12b6061a198bd3ba45
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-21 19:03:27 +0000

    Concatenate two strings w.r.t n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+int str_len(char *str);
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes to copy
+ * Return: concatenated char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len;
+
+	len = str_len(dest);
+	for (i = len, j = 0; j < n; i++, j++)
+	{
+		if (src[j] == '\0')
+			break;
+		else
+			dest[i] = src[j];
+	}
+	return (dest);
+}
+
+/**
+ * str_len - get length of string
+ * @str: string
+ * Return: string length
+ */
+int str_len(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
