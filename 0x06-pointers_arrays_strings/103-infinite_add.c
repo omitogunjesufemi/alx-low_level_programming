fef192616d7cd177398d03a2afbbf6d3bbcf3752
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-22 07:25:36 +0000

    Capitalize strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,66 +11,77 @@ void rev_string(char *s);
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i;
-	int str_to_digit_n1, str_to_digit_n2, sum_of_digit, remainder;
+	int str_to_digit_n1, str_to_digit_n2, sum_of_digit;
+	char digit_to_str;
+	int remainder = 0;
+	int count_n1 = 0;
+	int count_n2 = 0;
+	int count_r = 0;
 	int length_n1 = str_len(n1);
 	int length_n2 = str_len(n2);
 
 	rev_string(n1);
 	rev_string(n2);
 
-	if (length_n1 > size_r || length_n2 > size_r)
+	if (length_n1 >= size_r || length_n2 >= size_r)
 	{
-		r[0] = '0';
-		return (r);
+		return (0);
 	}
-	else
+
+	/**
+	 * For as long as there is a remainder,
+	 * and the length of either strings are yet to be reached
+	 */
+	while (count_n1 < length_n1 || count_n2 < length_n2 || remainder == 1)
 	{
-		if (length_n1 > length_n2)
+		/**
+		 * Use zero for addition once
+		 * the length has been reached
+		 */
+		if (count_n1 >= length_n1)
 		{
-			for (i = 0; i < length_n1; i++)
-			{
-				if (n2[i] != '\0')
-				{
-					str_to_digit_n1 = n1[1] - '0';
-					str_to_digit_n2 = n2[2] - '0';
-
-					/**
-					 * Add the two digits together and with
-					 * a remainder from previous calculation
-					 * if there is any.
-					 */
-					if (remainder > 0)
-					{
-						sum_of_digit = str_to_digit_n1 + str_to_digit_n2 + remainder;
-					}
-					else
-					{
-						sum_of_digit = str_to_digit_n1 + str_to_digit_n2;
-					}
-
-
-					/**
-					 * Checking for the remainder after
-					 * summing the two digits
-					 */
-					if (sum_of_digit > 9)
-					{
-						sum_of_digit -= 10;
-						remainder = 1;
-					}
-					else
-					{
-						remainder = 0;
-					}
-
-					r[i] = sum_of_digit + '0';
-				}
-				rev_string(r);
-			}
+			str_to_digit_n1 = 0;
 		}
-		return (r);
+		else
+		{
+			str_to_digit_n1 = n1[count_n1] - '0';
+		}
+
+		if (count_n2 >= length_n2)
+		{
+			str_to_digit_n2 = 0;
+		}
+		else
+		{
+			str_to_digit_n2 = n2[count_n2] - '0';
+		}
+
+		/**
+		 * Adding the str_to_digit together
+		 */
+		sum_of_digit = str_to_digit_n1 + str_to_digit_n2 + remainder;
+		/**
+		 * Checking for remainder
+		 */
+		if (sum_of_digit > 9)
+		{
+			sum_of_digit = sum_of_digit - 10;
+			remainder = 1;
+		}
+		else
+		{
+			remainder = 0;
+		}
+
+		digit_to_str = sum_of_digit + '0';
+		r[count_r] = digit_to_str;
+
+		count_n1++;
+		count_n2++;
+		count_r++;
 	}
+	rev_string(r);
+	return (r);
 }
 
 
