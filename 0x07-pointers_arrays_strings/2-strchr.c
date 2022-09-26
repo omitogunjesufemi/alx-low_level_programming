d668dd21d96c1aa6d025f4297f8baa0a5a18f20a
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-09-26 14:38:10 +0000

    Character search

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: the string to search
+ * @c: character to find
+ * Return: a pointer to the first occurence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int length_s, i;
+
+	length_s = 0;
+	while(s[length_s] != '\0')
+	{
+		length_s++;
+	}
+
+	for (i = 0; i < length_s; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,3 +15,11 @@ char *_memset(char *s, char b, unsigned int n);
  * @n: the number of bytes to fill the memory area
  */
 char *_memcpy(char *dest, char *src , unsigned int n);
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: the string to search
+ * @c: character to find
+ * Return: a pointer to the first occurence of the character
+ */
+char *_strchr(char *s, char c);
