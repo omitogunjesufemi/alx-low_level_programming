45c6ae4c09e7cde554008fe1c0ee5b9aa9840879
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-04 14:27:27 +0000

    Correction of alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,13 +28,12 @@ int **alloc_grid(int width, int height)
 
 		if (*(ptr + i) == NULL)
 		{
-			free(*(ptr + i));
+			for (j = 0; j < width; j++)
+				free(*(ptr + i));
+			free(ptr);
 			return (NULL);
 		}
-	}
 
-	for (i = 0; i < height; i++)
-	{
 		for (j = 0; j < width; j++)
 			*(*(ptr + i) + j) = 0;
 	}
