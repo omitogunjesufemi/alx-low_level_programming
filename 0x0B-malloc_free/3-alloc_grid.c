f8db44a31107d497c94468a81c9dd6af4700acc4
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-04 06:48:23 +0000

    Create 2D array with double pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = malloc(height * sizeof(int));
+	ptr = malloc(height * sizeof(int*));
 
 	for (i = 0; i < height; i++)
 	{
