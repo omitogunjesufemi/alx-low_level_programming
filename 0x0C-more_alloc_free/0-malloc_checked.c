1ebe6b1ec5303f2d94d6ab8e843f05cb8c677ec6
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-06 09:36:58 +0000

    Create and check memory allocation

---
+++ 0x0C-more_alloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - it allocates memory using malloc
+ * @b: the size of memory to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_alloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+
+/**
+ * malloc_checked - it allocates memory using malloc
+ * @b: the size of memory to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b);
+
+#endif
