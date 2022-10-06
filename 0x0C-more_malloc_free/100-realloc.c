945323b7ba55fbaabceffff75d62ca29fc21c17a
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-06 14:46:23 +0000

    Reallocate a memory block using free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (new_pointer);
 	}
 
-	if (new_size ==  0 && ptr != NULL)
+	if (new_size ==  0 || ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
