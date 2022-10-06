70ba440548243021473974ee26e65c813a88eecc
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-06 09:36:58 +0000

    Create and check memory allocation

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,39 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the size of memory to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b);
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: the number of bytes to copy from second string
+ * Return: a pointer to the null-terminanted concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of elements
+ * @size: the size of the type of elements in memory
+ * Return: a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/**
+ * array_range - creates an array of integers
+ * @min: smallest number in the array
+ * @max: largest number in the array
+ * Return: a pointer to the newly created array
+ */
+int *array_range(int min, int max);
+
+#endif
