8f28e5f368f3c1fba4e2f1b29723a080e7f8a0e5
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-07 09:01:53 +0000

    Correction on string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,13 +14,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
 	len_1 = strlen(s1);
 	len_2 = strlen(s2);
-
 	if (n >= len_2)
 		n = len_2;
 
