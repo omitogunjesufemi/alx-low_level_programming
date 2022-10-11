88c24714fe4b5d9aad941526759f70d50b834524
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-11 08:57:25 +0000

    Corrections on task 4 and 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,8 @@
 #include "dog.h"
+#include <string.h>
+
+int str_len(char *s);
+char *str_cpy(char *dest, char *src);
 
 /**
  * new_dog - creates a new dog
@@ -19,8 +23,61 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	dog_ptr->name = name;
+	dog_ptr->name = malloc(str_len(name) * sizeof(char));
+	dog_ptr->owner = malloc(str_len(owner) * sizeof(char));
+
+	if (dog_ptr->name == NULL)
+	{
+		free(dog_ptr->name);
+		return (NULL);
+	}
+
+	if (dog_ptr->owner == NULL)
+	{
+		free(dog_ptr->owner);
+		return (NULL);
+	}
+
+	str_cpy(dog_ptr->name, name);
 	dog_ptr->age = age;
-	dog_ptr->owner = owner;
+	str_cpy(dog_ptr->owner, owner);
 	return (dog_ptr);
+}
+
+/**
+ * str_cpy - copies the string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to where the string is copied to
+ */
+char *str_cpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = str_len(src);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[-1] = '\0';
+
+	return (dest);
+}
+
+/**
+ * str_len - the length of a string
+ * @s: string
+ * Return: length of string
+ */
+int str_len(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,9 +5,7 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
-	{
-		free(d);
-		d = NULL;
-	}
+	free(d->name);
+	free(d->owner);
+	free(d)
 }
