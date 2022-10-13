87039806b898cd43741fe78ffae403efdb1ac1c8
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-13 03:35:53 +0000

    Opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main method
+ * @argc: arguments count
+ * @argv: arguments
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	char *ptr;
+	int n = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ptr = (char *) &main;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%.2hhx ", ptr[i]);
+		else
+			printf("%.2hhx", ptr[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
