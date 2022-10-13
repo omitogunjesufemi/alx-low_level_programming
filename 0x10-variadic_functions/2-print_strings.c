2bed814035cf11bb08f4bd8228744e11af345f28
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-13 13:20:53 +0000

    Print strings and new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,12 +10,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list str_arg;
 	unsigned int i;
+	char *ptr;
 
 	va_start(str_arg, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(str_arg, char *));
+		ptr = va_arg(str_arg, char *);
+		if ( ptr != NULL)
+			printf("%s", ptr);
+		else
+			printf("(nil)");
 
 		if ((i != n - 1) && separator)
 			printf("%s", separator);
