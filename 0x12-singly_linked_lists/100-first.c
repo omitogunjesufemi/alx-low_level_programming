eb6b46a924c0795033a09629b1257305445e6ddd
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-21 07:35:33 +0000

    A call before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * _start_main - Do something before main
+ */
+void _start_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
