3a5c5b70685f05eb84d46b9f48d62ad4006ff0a5
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-25 19:04:36 +0000

    Insert a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -47,6 +47,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
+	if (idx == 0)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	free(new_node);
 	return (NULL);
 }
