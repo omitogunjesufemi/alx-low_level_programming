8d6779e24319fe5cdb650652554902bba0b57ac1
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-25 19:02:33 +0000

    Insert a new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: the address of the head node
+ * @idx: Given position starting from 0
+ * @n: the data of the new node
+ * Return: the address of the new node or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *prev;
+	listint_t *current;
+	unsigned int i;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head != NULL)
+	{
+		i = 0;
+		prev = NULL;
+		current = *head;
+		while (i < idx && current != NULL)
+		{
+			prev = current;
+			current = current->next;
+			i++;
+		}
+
+		if (i == idx && current != NULL)
+		{
+			new_node->next = current;
+			prev->next = new_node;
+			return (new_node);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+
+	*head = new_node;
+	return (new_node);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -89,4 +89,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
  */
 int sum_listint(listint_t *head);
 
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: the address of the head node
+ * @idx: Given position starting from 0
+ * @n: the data of the new node
+ * Return: the address of the new node or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
 #endif
