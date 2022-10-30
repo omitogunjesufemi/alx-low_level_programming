f0acb68d4bda96053fdcf94be7b7275acb6cbfd3
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-30 21:30:48 +0000

    Get the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit in a given index
+ * @n: the number
+ * @index: the index, starting from 0 of the bit you want to get
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n & (1 << index)) >> index);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -27,5 +27,12 @@ unsigned int binary_to_uint(const char *b);
  */
 void print_binary(unsigned long int n);
 
+/**
+ * get_bit - Returns the value of a bit in a given index
+ * @n: the number
+ * @index: the index, starting from 0 of the bit you want to get
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
