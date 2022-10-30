218a98067aa121e3ebef6da09011dedb53b70f46
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-30 23:18:17 +0000

    Get numbers of bit to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,26 +9,20 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	if (n && m)
+	unsigned long int k;
+	int F, count, position, bit_value;
+
+	k = n ^ m;
+	F = 1;
+	count = 0;
+	position = 0;
+	while (F != 0)
 	{
-		unsigned long int k;
-		int F, count, position, bit_value;
-
-		k = n ^ m;
-		F = 1;
-		count = 0;
-		position = 0;
-		while (F != 0)
-		{
-			bit_value = (k & (1 << position)) >> position;
-
-			if (bit_value != 0)
-				count++;
-			position++;
-			F = F << 1;
-		}
-		return (count);
+		bit_value = (k & (1 << position)) >> position;
+		if (bit_value != 0)
+			count++;
+		position++;
+		F = F << 1;
 	}
-
-	return (0);
+	return (count);
 }
