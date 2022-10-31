581273a67cf9eceac1356600b3f73bcda8514610
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-31 20:41:05 +0000

    Createa file with a particular permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,12 +4,12 @@ int str_len(char *s);
 /**
  * create_file - It creates a file
  * @filename: name of the file to be created
- * @text_context: a null terminated string
+ * @text_content: a null terminated string
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, fd,check;
+	int i, fd, check;
 	size_t len;
 	char *buffer;
 
