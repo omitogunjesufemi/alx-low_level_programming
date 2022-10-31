767d9af3f95e413f05a5576fd77ef52b3905ef89
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-31 16:53:11 +0000

    Setting up 0x15-file_io

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,6 @@
+# 0x15. C - File I/O
+>
+
+## Tasks and Description
+TASKS | DESCRIPTION
+--- | ---
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+#endif
