94be4359e07c22948cc256b28bce26926fa2bf64
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-10-31 19:35:17 +0000

    Read from a file and print to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,18 +16,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_CREAT | O_RDWR, 00600);
+
 	if (fd == -1)
 		return (0);
 	buffer = malloc((1 + letters) * sizeof(char));
+
 	if (buffer == NULL)
 		return (0);
 	count = read(fd, buffer, letters);
+
 	if (count == -1)
 		return (0);
 	count = write(STDOUT_FILENO, buffer, letters);
+
 	if (count == -1)
 		return (0);
 
+	free(buffer);
 	close(fd);
 	return (count);
 }
