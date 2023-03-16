f90f7726e0b3d29eed8081a9bfbf50973ccd2ddf
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-16 16:22:45 +0000

    Return length of linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -28,4 +28,11 @@ typedef struct dlistint_s
  */
 size_t print_dlistint(const dlistint_t *h);
 
+/**
+ * dlistint_len - Returns the number of elements in a linked list
+ * @h: Linked list
+ * Return: the number of elements in a linked list
+ */
+size_t dlistint_len(const dlistint_t *h);
+
 #endif
