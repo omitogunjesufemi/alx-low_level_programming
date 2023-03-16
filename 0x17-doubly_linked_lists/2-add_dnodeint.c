b543e97145e11547cde9698b86ec9163182e1007
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-16 16:43:18 +0000

    Adds a new node at the beginning of a list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,6 +20,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = NULL;
 
 	new_node->next = *head;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	(*head)->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
