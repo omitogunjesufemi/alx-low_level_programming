7395fa798db180d6a85064d56202adc6f3e06ca4
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2023-12-13 19:44:15 +0100

    Fixed edge cases for the printing for when output isn't found

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,10 +30,15 @@ int jump_search(int *array, size_t size, int value)
 		start = end;
 		end = end + ((size_t) sqrt(size));
 
-		if (start >= size)
+		if (start >= size - 1)
 		{
+			start = size - 1;
+			printf("Value checked array[%ld] = [%d]\n",
+			       start, array[start]);
 			printf("Value found between indexes [%ld] and [%ld]\n",
 			       start, end);
+			printf("Value checked array[%ld] = [%d]\n",
+			       start, array[start]);
 			return (-1);
 		}
 	}
